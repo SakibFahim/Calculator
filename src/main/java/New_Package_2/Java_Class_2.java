9029b3bb7309549294224854d209f5595745495f
SakibFahim <sakibfahim1999@gmail.com>
2022-05-09 19:55:51 +0600

    Change in Java File 2

--- src/main/java/New_Package_2/Java_Class_2.java
+++ src/main/java/New_Package_2/Java_Class_2.java
@@ -3,5 +3,46 @@ package New_Package_2;
 public class Java_Class_2 {
     string abc;
     int d;
-    
+    string p;
+    int r;
+
+
+    public Java_Class_2(string abc, int d, string p, int r) {
+        this.abc = abc;
+        this.d = d;
+        this.p = p;
+        this.r = r;
+    }
+
+    public string getAbc() {
+        return abc;
+    }
+
+    public void setAbc(string abc) {
+        this.abc = abc;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public string getP() {
+        return p;
+    }
+
+    public void setP(string p) {
+        this.p = p;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
 }
