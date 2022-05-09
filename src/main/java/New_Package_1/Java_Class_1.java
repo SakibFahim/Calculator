4c5ebc8971ef136f0fd387fdadef02ccdc7151ff
SakibFahim <sakibfahim1999@gmail.com>
2022-05-09 19:49:50 +0600

    Add New File

---
+++ src/main/java/New_Package_1/Java_Class_1.java
@@ -0,0 +1,28 @@
+package New_Package_1;
+
+public class Java_Class_1 {
+
+    string name;
+    int ID;
+
+    public Java_Class_1(string name, int ID) {
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+}
