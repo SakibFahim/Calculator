8cca2b963b527c8284735d0e8f92ca267c2e0048
SakibFahim <sakibfahim1999@gmail.com>
2022-05-09 19:57:52 +0600

    Change in Branch 2

--- src/main/java/New_Package_2/Java_Class_2.java
+++ src/main/java/New_Package_2/Java_Class_2.java
@@ -3,5 +3,65 @@ package New_Package_2;
 public class Java_Class_2 {
     string abc;
     int d;
-    
+    int a;
+    int b;
+    int c;
+    string def;
+
+    public Java_Class_2(string abc, int d, int a, int b, int c, string def) {
+        this.abc = abc;
+        this.d = d;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.def = def;
+    }
+
+    public string getAbc() {
+        return abc;
+    }
+
+    public void setAbc(string abc) {
+        this.abc = abc;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public string getDef() {
+        return def;
+    }
+
+    public void setDef(string def) {
+        this.def = def;
+    }
 }
